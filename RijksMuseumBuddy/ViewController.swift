02e142c3f4c0de1a6386376b02342c420d14610e
Ugur Unlu <vally10@live.com>
2021-08-28 10:18:09 +0200

    first commit

---
+++ RijksMuseumBuddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RijksMuseumBuddy
+//
+//  Created by Ugur Unlu on 8/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
