1f3da16eea8d91c094c9923a7e4f5736027a7b27
Ugur Unlu <vally10@live.com>
2021-08-28 11:34:10 +0200

    wip: add searchcell

---
+++ RijksMuseumBuddy/Views/SearchCell.swift
@@ -0,0 +1,17 @@
+//
+//  SearchCell.swift
+//  SearchCell
+//
+//  Created by Ugur Unlu on 8/28/21.
+//
+
+import UIKit
+
+class SearchCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
