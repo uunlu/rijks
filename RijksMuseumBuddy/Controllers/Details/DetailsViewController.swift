84f057f8dbaed359b0d0f2bfaa46781ecb0cf2d7
Ugur Unlu <vally10@live.com>
2021-08-28 23:28:41 +0200

    wip: add empty detailed view

---
+++ RijksMuseumBuddy/Controllers/Details/DetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  DetailsViewController
+//
+//  Created by Ugur Unlu on 8/28/21.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
