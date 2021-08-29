6b6f4792465bce4a10a5ce8a4ca62426002f1a09
Ugur Unlu <vally10@live.com>
2021-08-29 15:50:24 +0200

    fix: cache task to cancel download image

--- RijksMuseumBuddy/Utilities/ImageLoader.swift
+++ RijksMuseumBuddy/Utilities/ImageLoader.swift
@@ -11,18 +11,28 @@ import UIKit
 final class ImageLoader: NSObject {
     var cache: NSCache<NSString, UIImage>
     private var downloadTask: URLSessionDataTask?
+    private var runningRequests = [UUID: URLSessionDataTask]()
     
     init(cache: NSCache<NSString, UIImage> = NSCache()) {
         self.cache = cache
     }
     
-    func download(url: URL, completion: @escaping ((UIImage?) -> (Void))) {
+    @discardableResult
+    func download(url: URL, completion: @escaping ((UIImage?) -> (Void))) -> UUID? {
         if let cachedImage = cache.object(forKey: url.absoluteString as NSString) {
             completion(cachedImage)
         }
         
+        let uuid = UUID()
+        
         downloadTask = URLSession.shared.dataTask(with: url) { (data, response, error) -> Void in
+            defer {self.runningRequests.removeValue(forKey: uuid) }
+            guard nil == error else {
+                print("\(error!.localizedDescription)")
+                return
+            }
             guard let data = data, let image = UIImage(data: data) else {
+                print(url)
                 completion(nil)
                 return
             }
@@ -30,9 +40,17 @@ final class ImageLoader: NSObject {
             completion(image)
         }
         downloadTask?.resume()
+        runningRequests[uuid] = downloadTask
+        
+        return uuid
     }
     
     func cancel() {
         downloadTask?.cancel()
+    }
+    
+    func cancelLoad(_ uuid: UUID) {
+      runningRequests[uuid]?.cancel()
+      runningRequests.removeValue(forKey: uuid)
     }
 }
--- RijksMuseumBuddy/Views/ArtCell.swift
+++ RijksMuseumBuddy/Views/ArtCell.swift
@@ -21,8 +21,12 @@ class ArtCell: UICollectionViewCell {
     
     override func prepareForReuse() {
         imageView.image = nil
-        viewModel?.imageLoader?.cancel()
+        guard let id = imageDownloadID else { return }
+        viewModel?.imageLoader?.cancelLoad(id)
     }
+    
+    private var imageDownloadID: UUID?
+    static let identifier = "ArtCell"
     
     var viewModel: ArtDTO? {
         didSet{
@@ -30,17 +34,32 @@ class ArtCell: UICollectionViewCell {
             longTitle.text = viewModel?.longTitle
             
             guard let imageLink = viewModel?.imageLink, let imageURL = URL(string: imageLink) else {
+                self.imageView.image = UIImage(named: "Default")
                 return
             }
             imageView.contentMode = .scaleAspectFill
             imageView.translatesAutoresizingMaskIntoConstraints = false
             imageView.layer.cornerRadius = 15
-            guard let imageLoader = viewModel?.imageLoader else { return }
-            imageLoader.download(url: imageURL, completion: { image in
-                DispatchQueue.main.async {
-                    self.imageView.image = image
-                }
-            })
+            loadImage(imageURL: imageURL)
         }
     }
+    
+    private func loadImage(imageURL: URL) {
+        guard let imageLoader = viewModel?.imageLoader else { return }
+        // TODO: A retry mechanism with finite number of trials of downloading images should be implemented.
+        imageDownloadID = imageLoader.download(url: imageURL, completion: { image in
+            if image == nil {
+                self.imageView.image = UIImage(named: "Default")
+                DispatchQueue.global(qos: .background).asyncAfter(deadline: .now()+1){
+                    imageLoader.cancelLoad(self.imageDownloadID!)
+                    self.loadImage(imageURL: imageURL)
+                    return
+                }
+            }
+            DispatchQueue.main.async {
+                    self.imageView.image = image
+            }
+        })
+    }
 }
+
